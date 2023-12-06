7127d9e5630573f0652a029e4164266c5b4d8880
Ryan Joesting <rmjoesting@crimson.ua.edu>
2023-12-06 16:52:30 -0600

    Updated Twitter URL code

--- src/App.js
+++ src/App.js
@@ -1,52 +1,96 @@
 import './App.css';
 import { useState, useEffect } from 'react';
-import QuoteCard from './QuoteCard'
+import QuoteCard from './QuoteCard';
 
 function App() {
-
   const [quotes, setQuotes] = useState([]);
   const [randomQuote, setRandomQuote] = useState("");
-  const [colors, setColors] = useState(['#ff80ed', '#065535', '#ffc0cb', '#008080', '#40e0d0', '#b0e0e6']);
+  const colors = [
+    '#ff80ed',
+    '#065535',
+    '#ffc0cb', 
+    '#008080', 
+    '#40e0d0',
+    '#b0e0e6', 
+    '#ff6347',
+    '#1e90ff',
+    '#32cd32',
+    '#ff4500',
+    '#9370db',
+    '#00ced1',
+    '#ffa500',
+    '#8a2be2',
+    '#ff69b4',
+    '#00fa9a',
+    '#4169e1',
+    '#ff8c00',
+    '#20b2aa',
+    '#8b008b',
+    '#ff1493'];
   const [randomColor, setRandomColor] = useState('');
+  const [tweetQuoteURL, setTweetQuoteURL] = useState('');
 
-  useEffect( () => {
+  useEffect(() => {
     async function fetchQuotes() {
-      const response = await fetch("https://type.fit/api/quotes");
-      const data = await response.json();
-      setQuotes(data);
-      let index = Math.floor(Math.random() * data.length);
-      setRandomQuote({
-        text: data[index].text,
-        author: data[index].author.replace(/,?\s?type.fit/, '')
-      });
-    };
+      try {
+        const response = await fetch("https://type.fit/api/quotes");
+        const data = await response.json();
+        setQuotes(data);
+        let index = Math.floor(Math.random() * data.length);
+        let author = data[index].author || 'Unknown';
+
+        // Check if author is a non-empty string before applying replace
+        if (typeof author === 'string' && author.trim() !== '') {
+          author = author.replace(/,?\s?type.fit/, ''); // Removes annoying 'type.fit' from the end of the author name (from API)
+        }
+        setRandomQuote({
+          text: data[index].text,
+          author: author
+        });
+        let twURL = data[index].text;
+        setTweetQuoteURL(twURL.replace(/\s/g, '%20'));
+      } catch (error) {
+        console.error("Error fetching quotes:", error);
+      }
+    }
     fetchQuotes();
     updateBgColor();
   }, []);
 
-  useEffect( () => {
+  useEffect(() => {
     updateBgColor();
-  }, [randomQuote])
+  }, [randomQuote]);
 
   const updateBgColor = () => {
     let index = Math.floor(Math.random() * colors.length);
     setRandomColor(colors[index]);
-  }
+  };
+  
+  useEffect(() => {
+    console.log(tweetQuoteURL);
+  }, [tweetQuoteURL]);
 
   const newQuote = () => {
     let index = Math.floor(Math.random() * quotes.length);
-    let _author = quotes[index].author.replace(/,?\s?type.fit/, ''); //removes annoying 'type.fit' from end of author name (from API)
-    if (_author == '') _author = 'Unknown';
+    let _author = quotes[index].author || 'Unknown';
+
+    // Check if _author is a non-empty string before applying replace
+    if (typeof _author === 'string' && _author.trim() !== '') {
+      _author = _author.replace(/,?\s?type.fit/, ''); // Removes annoying 'type.fit' from the end of the author name (from API)
+    }
+
     setRandomQuote({
       text: quotes[index].text,
       author: _author
     });
-  }
+    let twURL = quotes[index].text;
+    setTweetQuoteURL(twURL.replace(/\s/g, '%20'));
+  };
 
   return (
-    <div style={{backgroundColor: randomColor}}>
+    <div style={{ backgroundColor: randomColor }}>
       <div className="App container d-flex justify-content-center vh-100 align-items-center">
-        <QuoteCard text={randomQuote.text} author={randomQuote.author} newQuote={newQuote}/>
+        <QuoteCard text={randomQuote.text} author={randomQuote.author} newQuote={newQuote} twitterIntent={tweetQuoteURL}/>
       </div>
     </div>
   );
--- src/QuoteCard.js
+++ src/QuoteCard.js
@@ -1,22 +1,24 @@
 import { useState } from 'react'
 
-function QuoteCard({ text, author, newQuote }) {
+function QuoteCard({ text, author, newQuote, twitterIntent }) {
 
   return (
-    <div className="card bg-white" id="quote-box" style={{width: 46 + 'rem'}}>
+    <div className="card bg-white shadow-lg border border-5 border-dark" id="quote-box" style={{width: 20 + 'rem'}}>
         <div className="card-body">
           <p className="card-text" id="text">{text}</p>
           <h6 className="card-title" id="author">by {author}</h6>
           <div className="row card-footer bg-white">
             <div className='col-md-3'>
-              <button className='btn btn-dark'>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-twitter-x" viewBox="0 0 16 16">
-                  <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865l8.875 11.633Z"/>
-                </svg>
-              </button>
+                <button className='btn btn-light'>
+                  <a id='tweet-quote' target='_blank' href={'https://twitter.com/intent/tweet?text=' + twitterIntent}>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-twitter-x" viewBox="0 0 16 16">
+                          <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865l8.875 11.633Z"/>
+                      </svg>
+                  </a>
+                </button>
             </div>
             <div className='col-md-6'></div>
-            <div className='col-md-3'><button onClick={newQuote} className='btn btn-dark'>New Quote</button></div>
+            <div className='col-md-3'><button onClick={newQuote} id='new-quote' className='btn btn-dark'>New Quote</button></div>
             </div>         
         </div>
       </div>
